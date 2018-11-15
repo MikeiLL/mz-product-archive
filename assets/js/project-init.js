c8a7f510bcc2e7cb9fab66c964fe76782234cf17
Mike iLL Kilmer <mike@mzoo.org>
2018-11-14 20:09:25 -0600

    Initialize photoswipe on gallery as well as portfolio pages.

--- assets/js/project-init.js
+++ assets/js/project-init.js
@@ -23,11 +23,75 @@
 
   });
   
-  $('.portfolio-gallery').flickity({
+    $gallery.on('staticClick.flickity', function(event, pointer, cellElement, cellIndex) {
+    if (!cellElement) {
+      return;
+    }
+
+    // Photoswipe functions
+    var openPhotoSwipe = function() {
+      var pswpElement = document.querySelectorAll('.pswp')[0];
+
+      // build items array
+
+      var items = $.map($(".gallery").find("img"), function(el) {
+        return {
+          "src": el.getAttribute('data-src'),
+          "w":   el.getAttribute('data-width'),
+          "h":   el.getAttribute('data-height')
+        }
+      });
+
+      var options = {
+      	history: false,
+        index: cellIndex
+      };
+
+      var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+      gallery.init();
+
+    };
+
+    openPhotoSwipe();
+  });
+  
+   var $portfolio_gallery = $('.portfolio-gallery').flickity({
     imagesLoaded: true,
     percentPosition: false,
     wrapAround: true,
     pageDots: true
+  });
+  
+  $portfolio_gallery.on('staticClick.flickity', function(event, pointer, cellElement, cellIndex) {
+    if (!cellElement) {
+      return;
+    }
+
+    // Photoswipe functions
+    var openPhotoSwipe = function() {
+      var pswpElement = document.querySelectorAll('.pswp')[0];
+
+      // build items array
+
+      var items = $.map($(".portfolio-gallery").find("img"), function(el) {
+        return {
+          "src": el.getAttribute('data-src'),
+          "w":   el.getAttribute('data-width'),
+          "h":   el.getAttribute('data-height')
+        }
+      });
+
+      var options = {
+      	history: false,
+        index: cellIndex
+      };
+
+      var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+      gallery.init();
+
+    };
+
+    openPhotoSwipe();
   });
 
 
