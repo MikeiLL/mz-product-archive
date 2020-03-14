bc01dd49493fceba1301f79cc92b1a699a468d96
Mike iLL Kilmer <mike@mzoo.org>
2020-03-13 19:08:01 -0500

    Hack to display something in thumbnail when no featured image present.

--- templates/content-thumb.php
+++ templates/content-thumb.php
@@ -11,29 +11,22 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly  ?>
 
-	<?php //do_action( 'projects_before_loop_item' ); ?>
+<?php
+/**
+* projects_loop_item hook
+*
+* @hooked projects_template_loop_project_thumbnail - 10
+* @hooked projects_template_loop_project_title - 20
+*/
+//do_action( 'projects_loop_item' );
+$project_thumb = projects_get_project_thumbnail('project-thumbnail');
 
+if (!empty($project_thumb)):
+	  echo $project_thumb;
+else:
+	// This is a hack.
+	echo '<img style="width:100px;height:100px" src="' . mzoo_project_archive_default_image() . '"/>';
+endif;
 
-		<?php
-			/**
-			 * projects_loop_item hook
-			 *
-			 * @hooked projects_template_loop_project_thumbnail - 10
-			 * @hooked projects_template_loop_project_title - 20
-			 */
-			//do_action( 'projects_loop_item' );
-			$project_thumb = projects_get_project_thumbnail('project-thumbnail');
-
-			if (!empty($project_thumb)):
-
-			  echo $project_thumb;
-
-      endif;
-		/**
-		 * projects_after_loop_item hook
-		 *
-		 * @hooked projects_template_short_description - 10
-		 */
-		//do_action( 'projects_after_loop_item' );
-	?>
+?>
 
